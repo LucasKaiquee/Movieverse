d9cad8dd4039d9d9903e3dabdc703f189c19d896
Lucas Kaique <lucaskaique743@gmail.com>
2023-06-11 15:19:28 -0300

    feat: add BestMovies and Card components

--- src/components/BestMovies.jsx
+++ src/components/BestMovies.jsx
@@ -1,40 +1,65 @@
-import axios from "axios"
-import { useState } from "react"
-
 import Card from "./Card"
+import { useState, useEffect} from "react"
+import { FaStar } from "react-icons/fa";
 
 import "./BestMovies.css"
 
 const BestMovies = () => {
+    const [changeResource, setChangeResource] = useState([]);
+    const [type, setType] = useState('')
 
-    const besttitle = [
-        'The Shawshank Redemption', 
-        'The Godfather', 
-        'The Godfather: Part II', 
-        '12 Angry Men', 
-        "Schindler's List", 
-        'The Lord of the Rings: The Return of the King',
-        'Pulp Fiction',
-        'The Good, the Bad and the Ugly',
-        'The Lord of the Rings: The Fellowship of the Ring'
-    ]
+    const typeFilm = 'Filmes'
+    const typeSerie = 'Séries'
 
+    const bestTitles = [
+      'The Shawshank Redemption',
+      'The Godfather',
+      'The Godfather: Part II',
+      '12 Angry Men',
+      "Schindler's List",
+      'The Lord of the Rings: The Return of the King',
+      'Pulp Fiction',
+      'The Good, the Bad and the Ugly',
+      'The Lord of the Rings: The Fellowship of the Ring'
+    ];
+  
+    const bestSeries = [
+      'Breaking Bad',
+      'Planet Earth II',
+      'Band of Brothers',
+      'Game of Thrones',
+      'Chernobyl',
+      'The Wire',
+      'Our Planet',
+      'The Sopranos',
+      'Rick and Morty'
+    ];
+  
+    const handleChangeResource = (titles, type) => {
+      setChangeResource(titles);
+      setType(type)
+    };
+  
+    useEffect(() => {
+      handleChangeResource(bestTitles, typeFilm);
+    }, []); 
+  
     return (
-        <>
-            <h1>Melhores Filmes</h1>
-            <div className="container-movies">
-                <Card bestTitleMovie={besttitle[0]} />
-                <Card bestTitleMovie={besttitle[1]} />
-                <Card bestTitleMovie={besttitle[2]} />
-                <Card bestTitleMovie={besttitle[3]} />
-                <Card bestTitleMovie={besttitle[4]} />
-                <Card bestTitleMovie={besttitle[5]} />
-                <Card bestTitleMovie={besttitle[6]} />
-                <Card bestTitleMovie={besttitle[7]} />
-                <Card bestTitleMovie={besttitle[8]} />
-            </div>
-        </>
-    )
-}
+      <>
+        <h1 className="best-h">Melhores {type}</h1>
+        <p className="best-p"><FaStar color="#FFFF00" size={20}/> Esses são os Títulos melhores avaliados no IMDB para {type}</p>
+        <div className="select-best">
+          <button onClick={() => handleChangeResource(bestTitles, typeFilm)}>Filmes</button>
+          <button onClick={() => handleChangeResource(bestSeries, typeSerie)}>Séries</button>
+        </div>
+        <div className="container-movies">
+          {changeResource.map((title, index) => (
+            <Card key={index} bestTitleMovie={title} />
+          ))}
+        </div>
+      </>
+    );
+  };
+  
 
 export default BestMovies
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,15 @@
+/* eslint-disable react/prop-types */
 import axios from "axios"
 import { useState } from "react";
+import { FaStar } from "react-icons/fa";
+import TemplateButton from "./TemplateButton";
 
 import "./Card.css"
 
 const Card = (props) => {
     const apiKey = 'a4afc84b';
     const [title, setTitle] = useState('')
+    const [titleRating, setTitleRating] = useState('')
 
     const Movie = () => {
         axios
@@ -13,6 +17,7 @@ const Card = (props) => {
           .then((response) => {
             console.log(response.data);
             setTitle(response.data.Poster)
+            setTitleRating(response.data.imdbRating)
           })
           .catch((error) => {
             console.error(error);
@@ -25,7 +30,10 @@ const Card = (props) => {
         <>
             <div className="container-card">
                 <img src={title} />
-                
+                <div className="content-card">
+                  <p><FaStar color="#FFFF00" /> {titleRating}</p>
+                  <TemplateButton />
+                </div>
             </div>
         </>
     )
