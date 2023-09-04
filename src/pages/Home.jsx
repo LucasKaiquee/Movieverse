7467a82abcf558a79b7bf80023eb59bad5c2a528
LucasKaiquee <lucaskaique743@gmail.com>
2023-09-04 10:47:38 -0300

    Feat: new section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import Navbar from "../components/Navbar";
 import BestMovies from "../components/BestMovies";
+import InputMovie from "../components/InputMovie";
 
 import './Home.css'
 
@@ -8,7 +9,18 @@ const Home = () => {
     return (
         <>
             <Navbar/>
-            <BestMovies/>
+            <header className="header">
+                <div className="container-header">  
+                    <h1>Bem-vindo ao MovieVerse</h1>
+                    <h3>Descubra Seu Filme Perfeito</h3>
+                    <p>No Movie Verse, sua jornada cinematográfica começa aqui! Nosso buscador de filmes é sua ferramenta confiável para descobrir o filme perfeito para cada ocasião. Com uma vasta coleção de títulos e informações detalhadas, estamos aqui para ajudá-lo a encontrar a próxima história que vai cativar sua imaginação.</p>
+
+                    <InputMovie />
+                </div>
+
+            </header>
+
+            <BestMovies />
             
             <footer>
                 <h4>Lucas Kaique &copy; 2023</h4>
