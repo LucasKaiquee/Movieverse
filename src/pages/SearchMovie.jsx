bf6f42242017089ad68cb84e6037afed0281b502
LucasKaiquee <lucaskaique743@gmail.com>
2023-10-07 22:44:53 -0300

    Feat: SearchMovie component

--- src/pages/SearchMovie.jsx
+++ src/pages/SearchMovie.jsx
@@ -1,6 +1,14 @@
 import { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
+
+import Card from '../components/Card';
+import InputMovie from '../components/InputMovie';
+import Navbar from '../components/Navbar';
+
 import axios from 'axios';
+
+import './SearchMovie.css'
+import logo1 from "../assets/imgs/VectorLogo1.svg"
 
 const SearchMovie = () => {
   const apiKey = import.meta.env.VITE_API_KEY;
@@ -8,19 +16,22 @@ const SearchMovie = () => {
 
   const location = useLocation();
   const state = location.state;
+  console.log(state)
 
-  const [result, setResult] = useState('');
+  const [result, setResult] = useState([]);
 
   const getMovie = (url) => {
     axios
       .get(url)
       .then((response) => {
-        setResult(response.data);
+        setResult(response.data.results);
       })
       .catch((error) => {
         console.error(error);
       });
   };
+
+  console.log(result)
 
   useEffect(() => {
     const searchWithQueryURL = `${searchUrl}?${apiKey}&query=${state}`;
@@ -28,8 +39,32 @@ const SearchMovie = () => {
   }, [state]);
 
   return (
-    <section>
-      <div className="container-movies">//resolver o problema do result</div>
+    <section >
+      <Navbar />
+      <div className='search-movie-container'>
+        <h3>Resultados para:</h3>
+        <p className='title-state'>{state}</p>
+        <InputMovie pageState={'Search'}/>
+        <div className='container-movies'>
+          {result.map((result, index) =>(
+            <Card  
+              key={index} 
+              path={result.poster_path} 
+              title={result.title}
+              voteAverage={result.vote_average}
+              id={result.id}
+            />
+          ))}
+        </div>
+      </div>
+
+      <footer>
+        <div className="container-footer">
+          <img src={logo1} alt="Logotipo" />
+            <span>Movie Verse</span>
+        </div>
+        <h4>Copyright © 2023 MovieVerse. Todos os direitos reservados.</h4>
+        </footer>
     </section>
   );
 };
