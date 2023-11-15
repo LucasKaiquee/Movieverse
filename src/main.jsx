fe5db0a17786e764a80ca18347ed43470837a972
LucasKaiquee <lucaskaique743@gmail.com>
2023-11-15 16:00:18 -0300

    feat: add lib material tailwind

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
+import { ThemeProvider } from "@material-tailwind/react";
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+    <ThemeProvider>
       <App />
+    </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
