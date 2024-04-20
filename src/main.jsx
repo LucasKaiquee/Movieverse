5ce9cddce1d8e282a7f08b642a90cb6a8efa0ba6
LucasKaiquee <lucaskaique743@gmail.com>
2024-04-19 22:07:44 -0300

    organizing styles into folders

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
 
+import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from "@material-tailwind/react";
 
-import './index.css'
+import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-import withMT from "@material-tailwind/react/utils/withMT"
-
-export default withMT({
+export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -10,4 +8,4 @@ export default withMT({
     extend: {},
   },
   plugins: [],
-})
+}
